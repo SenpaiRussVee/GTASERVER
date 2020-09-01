2bd03d9b9a45063f0b03554a3514bd516044a081
SenpaiRussVee <70479633+SenpaiRussVee@users.noreply.github.com>
2020-09-01 20:22:28 +0800

    Add files via upload

---
+++ Bodyguard/Client/Main.cs
@@ -0,0 +1,54 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            API.RegisterCommand("test",new Action(TestCommand), false );
+        }
+
+        private async static void TestCommand()
+        {
+            Ped player = Game.Player.Character;
+            API.RequestModel((uint)PedHash.ChemSec01SMM);
+            while(!API.HasModelLoaded((uint)PedHash.ChemSec01SMM))
+            {
+                Debug.WriteLine("Waiting for model to load");
+                await BaseScript.Delay(100);
+            }
+            Ped bguard = await World.CreatePed(PedHash.ChemSec01SMM,player.Position + (player.ForwardVector * 4));
+            bguard.Task.LookAt(player);
+            API.SetPedAsGroupMember(bguard.Handle, API.GetPedGroupIndex(player.Handle));
+            API.SetPedCombatAbility(bguard.Handle, 1);
+            SendMessage("[SERVER]", "Your bodyguard spawned!", 255, 0, 0);
+            API.GiveWeaponToPed(bguard.Handle, (uint)WeaponHash.APPistol, 100, false, true);
+            bguard.PlayAmbientSpeech("GENERIC_HI");
+
+        }
+
+
+
+
+        public static void SendMessage(string title, string message, int r, int g, int b)
+        {
+            var msg = new Dictionary<string, object>
+            {
+                ["color"] = new[] { r, g, b },
+                ["args"] = new[] { title, message }
+            };
+            TriggerEvent("chat:addMessage", msg);
+        }
+
+
+
+
+    }
+}
---
+++ carspawn/carSpawn/carSpawn.cs
@@ -0,0 +1,49 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carSpawn
+{
+    public class carSpawn : BaseScript
+    {
+        public carSpawn()
+        {
+            API.RegisterCommand("spawnlambonak", new Action<int, List<object>, string>(async(src, args, raw) =>
+            {
+                var car = new Model(VehicleHash.Zentorno);
+
+               var ferari =  await World.CreateVehicle(car, Game.PlayerPed.GetOffsetPosition(new Vector3(0f,5f,1f)), Game.PlayerPed.Heading);
+                ferari.NeedsToBeHotwired = false;
+                
+
+            }), false);
+        }
+
+
+
+
+
+
+
+
+        public static void SendMessage(string title, string message, int r, int g, int b)
+        {
+            var msg = new Dictionary<string, object>
+            {
+                ["color"] = new[] { r, g, b },
+                ["args"] = new[] { title, message }
+            };
+            TriggerEvent("chat:addMessage", msg);
+        }
+
+    }
+}
+
+
+
+
+
